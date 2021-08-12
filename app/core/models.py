83fccdaf9dc12c8ca9ab4366874a3c94672c4535
Kyle Price <k.price.la@gmail.com>
2021-08-12 14:39:42 -0400

    fix flake8 style issues

--- app/core/models.py
+++ app/core/models.py
@@ -16,7 +16,6 @@ class UserManager(BaseUserManager):
 
         return user
 
-
     def create_superuser(self, email: str, password: str):
         '''Creates and saves a new super user'''
         user = self.create_user(email, password)
@@ -39,4 +38,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
